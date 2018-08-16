6ac629a22ee823fea37e400896866a98576f2af9
Nathan Walters <nwalters512@gmail.com>
2018-08-15 19:07:55 -0700

    Remove reference to unused dependency

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var compression = require('compression');
-var methodOverride = require('method-override');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
@@ -42,7 +41,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
-app.use(methodOverride('_method'));
 app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
